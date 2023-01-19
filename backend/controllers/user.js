3b5ec0d93e24185d6bb02444b4d674a3e3e4d370
Damithkumara <damithkumararoxx55@gmail.com>
2023-01-19 15:23:39 +0530

    change format spacing

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,26 +1,26 @@
-import {db} from '../db.js';
+import { db } from '../db.js';
 
-export const getAllComments = (_,res) => {
+export const getAllComments = (_, res) => {
     const q = "SELECT * FROM userdata";
 
-    db.query(q,(err,data) => {
-        if(err) return res.json(err);
+    db.query(q, (err, data) => {
+        if (err) return res.json(err);
 
         return res.status(200).json(data);
     });
 };
 
-export const addComment = (req,res) => {
+export const addComment = (req, res) => {
     const q = "INSERT INTO userdata(`comment`) VALUES(?)";
 
     const values = [
 
         req.body.comment,
-        
+
     ];
 
-    db.query(q,[values],(err) => {
-        if(err) return res.json(err);
+    db.query(q, [values], (err) => {
+        if (err) return res.json(err);
 
         return res.status(200).json("insert done");
     });
@@ -29,15 +29,15 @@ export const addComment = (req,res) => {
 
 //update upvote
 
-export const updateUpvote = (req,res) => {
+export const updateUpvote = (req, res) => {
     const q = "UPDATE userdetails set `votevalue` = ?  where `id` = ? "
 
     const values = [
         req.body.btnupvote,
     ];
 
-    db.query(q,[...values, req.params.id],(err) => {
-        if(err) return res.json(err);
+    db.query(q, [...values, req.params.id], (err) => {
+        if (err) return res.json(err);
 
         return res.status(200).json("update is completed");
     });
